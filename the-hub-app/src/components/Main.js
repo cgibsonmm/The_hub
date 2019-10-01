81303931d0cfe3645d0663924dd1aad2e72a821f
Corey Gibson <cgibsonmmdev@gmail.com>
2019-10-01 12:21:05 -0400

    started building components

---
+++ the-hub-app/src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import StoryList from './StoryList'
+
+const Main = () => {
+  return (
+    <main>
+      <Sidebar />
+      <StoryList />
+    </main>
+
+  )
+}
+
+export default Main;
---
+++ the-hub-app/src/components/StoryList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const StoryList = () => {
+  return (
+    <ul></ul>
+  )
+}
+
+export default StoryList;
